d803cdb7e128cc4cf6606d05a03270a215ae056f
Crocodil1976 <151015725+Crocodil1976@users.noreply.github.com>
2025-12-05 15:38:14 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,7 +1,6 @@
 # Настройка версии прошивки
 sed -i 's/^FIRMWARE_ROOTFS_VER.*/FIRMWARE_ROOTFS_VER=3.9L/' padavan-ng/trunk/versions.inc
-sed -i 's/^FIRMWARE_BUILDS_VER.*/FIRMWARE_BUILDS_VER=102/' padavan-ng/trunk/versions.inc
-sed -i 's/^FIRMWARE_BUILDS_REV.*/FIRMWARE_BUILDS_REV=C001_$(shell git rev-parse --short HEAD 2>/dev/null)/' padavan-ng/trunk/versions.inc
+sed -i 's/^FIRMWARE_BUILDS_VER.*/FIRMWARE_BUILDS_VER=103/' padavan-ng/trunk/versions.inc
 
 # Настройка версии Zapret
 sed -i 's/^SRC_VER.*/SRC_VER = 72.3/' padavan-ng/trunk/user/nfqws/Makefile
