946f8b3c45f1e15d55eefc03fca1cd618fb2ef3b
Crocodil1976 <151015725+Crocodil1976@users.noreply.github.com>
2025-12-11 14:00:39 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,8 +1,8 @@
 # Настройка версии прошивки
 sed -i 's/^FIRMWARE_ROOTFS_VER.*/FIRMWARE_ROOTFS_VER=3.9L/' padavan-ng/trunk/versions.inc
-sed -i 's/^FIRMWARE_BUILDS_VER.*/FIRMWARE_BUILDS_VER=103/' padavan-ng/trunk/versions.inc
+sed -i 's/^FIRMWARE_BUILDS_VER.*/FIRMWARE_BUILDS_VER=104/' padavan-ng/trunk/versions.inc
 
 # Настройка версии Zapret
-sed -i 's/^SRC_VER.*/SRC_VER = 72.3/' padavan-ng/trunk/user/nfqws/Makefile
+sed -i 's/^SRC_VER.*/SRC_VER = 72.4/' padavan-ng/trunk/user/nfqws/Makefile
 cd padavan-ng/trunk/user/nfqws
 find . -maxdepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
