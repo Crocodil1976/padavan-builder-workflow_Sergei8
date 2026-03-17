260a9c6934d06ace5a0ba60dfee56b2eb095ba5b
Crocodil1976 <151015725+Crocodil1976@users.noreply.github.com>
2026-03-17 15:22:32 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -3,9 +3,9 @@ sed -i 's/^FIRMWARE_ROOTFS_VER.*/FIRMWARE_ROOTFS_VER=3.9L/' padavan-ng/trunk/ver
 sed -i 's/^FIRMWARE_BUILDS_VER.*/FIRMWARE_BUILDS_VER=105/' padavan-ng/trunk/versions.inc
 
 # Настройка версии Zapret
-sed -i 's/^SRC_VER.*/SRC_VER = 72.10/' padavan-ng/trunk/user/nfqws/Makefile
-cd padavan-ng/trunk/user/nfqws
-find . -maxdepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
+#sed -i 's/^SRC_VER.*/SRC_VER = 72.10/' padavan-ng/trunk/user/nfqws/Makefile
+#cd padavan-ng/trunk/user/nfqws
+#find . -maxdepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
 
 # Установка последней версии zapret
 #ZAPRET_REPO="https://github.com/bol-van/zapret.git"
@@ -14,3 +14,12 @@ find . -maxdepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm 
 #sed -i "s/^SRC_VER.*/SRC_VER = $ZAPRET_VER/g" padavan-ng/trunk/user/nfqws/Makefile
 #cd padavan-ng/trunk/user/nfqws
 #find . -maxdepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
+
+# Установка последней версии zapret
+ZAPRET_REPO="https://github.com/bol-van/zapret.git"
+ZAPRET_TAGS=$(git ls-remote --tags "$ZAPRET_REPO" | awk '{print $2}' | sed 's/refs\/tags\///g')
+ZAPRET_VER=$(echo "$ZAPRET_TAGS" | sort -V | tail -n 1 | sed 's/^.//')
+sed -i "s/^SRC_VER.*/SRC_VER = $ZAPRET_VER/g" padavan-ng/trunk/user/nfqws/Makefile
+cd padavan-ng/trunk/user/nfqws
+curl -o patches/firmware-specific.patch https://raw.githubusercontent.com/EdvardBill/npzp/refs/heads/main/firmware-specific.patch
+find . -maxdepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
